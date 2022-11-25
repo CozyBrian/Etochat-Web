795e5eef6f6730b3f91c65f0fc57445dd0d09ea0
CozyBrian <dragonbrian33@gmail.com>
2022-11-25 20:05:12 +0000

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,16 @@ import Peer, { MediaConnection } from "peerjs";
 import { PSUserType } from "./@types";
 import { useAppSelector } from "./hooks";
 
-const socket = io(`https://etochat.onrender.com`);
+// const DEVENV = "192.168.11.34";
+// const DEVENVVV = "172.20.10.2";
+const DEVENVV = "localhost";
+
+const socket = io(`${DEVENVV}:3001`);
 const peer = new Peer({
+  host: DEVENVV,
+  port: 3001,
+  path: "/peer",
+  debug: 0,
   secure: false,
   config: {
     iceServers: [
